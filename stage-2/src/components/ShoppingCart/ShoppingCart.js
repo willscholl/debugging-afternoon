2f114009e85bbb9411c532aba0036c81eca26fd0
William Scholl <willscholl@me.com>
2019-02-26 15:11:28 -0700

    working hard or hardly working?

--- stage-2/src/components/ShoppingCart/ShoppingCart.js
+++ stage-2/src/components/ShoppingCart/ShoppingCart.js
@@ -18,16 +18,18 @@ class ShoppingCart extends Component {
 
     render() {
         let shoppingCartDisplay = this.state.shoppingCart.map((element, index) => {
-            <div className="shopping-cart-product-container">
-                <img src={element.image} alt="" />
-                <div className="shopping-cart-info">
-                    <h2>{element.title}</h2>
-                    <h2>{"$" + element.price + ".00"}</h2>
-                    <div className="shopping-cart-button-container">
-                        <button className="shopping-cart-button" onClick={() => this.props.removeFromShoppingCart(element)}>Remove From Shopping Cart</button>
+            return (
+                <div className="shopping-cart-product-container">
+                    <img src={element.image} alt="" />
+                    <div className="shopping-cart-info">
+                        <h2>{element.title}</h2>
+                        <h2>{"$" + element.price + ".00"}</h2>
+                        <div className="shopping-cart-button-container">
+                            <button className="shopping-cart-button" onClick={() => this.props.removeFromShoppingCart(element)}>Remove From Shopping Cart</button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            )
         })
         return (
             <div className="shopping-cart-container">
--- stage-4/src/redux/store.js
+++ stage-4/src/redux/store.js
@@ -1,5 +1,5 @@
-import { createStore, applyMiddleware } from 'redux';
-import reducer from './reducer';
-import reduxPromiseMiddleware from 'redux-promise-middleware';
+import { createStore, applyMiddleware } from "redux";
+import reducer from "./reducer";
+import promise from "redux-promise-middleware";
 
-export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
+export default createStore(reducer, applyMiddleware(promise()));
